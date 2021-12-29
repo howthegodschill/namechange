570bdba3f3918661a5c72b1388ba7b1aa58151fc
howthegodschill <41914490+howthegodschill@users.noreply.github.com>
2021-12-29 11:45:35 -0800

    CHANGED COMPUTERNAME VARIABLE
    I didn't like the variable computerName, I think DeviceName is better

--- namechange.sh
+++ namechange.sh
@@ -2,18 +2,18 @@
 # This script will prompt the user for input,
 # then quickly change all of the necessary names for a Mac
 
-ComputerName=`/usr/bin/osascript << EOT
+DeviceName=`/usr/bin/osascript << EOT
 tell application "System Events"
     activate
-    set ComputerName to text returned of (display dialog "Please enter the user's name and ID number" default answer "" with icon 2)
+    set DeviceName to text returned of (display dialog "Please enter the user's name and ID number" default answer "" with icon 2)
 end tell
 EOT`
 
 #Set New Computer Name
-echo $ComputerName
-scutil --set HostName $ComputerName
-scutil --set LocalHostName $ComputerName
-scutil --set ComputerName $ComputerName
+echo $DeviceName
+scutil --set HostName $DeviceName
+scutil --set LocalHostName $DeviceName
+scutil --set DeviceName $DeviceName
 
 /usr/bin/osascript -e \
 'tell application "System Events" to (display dialog "Name change successful!" buttons {"OK"} default button "OK" with icon 2)'
